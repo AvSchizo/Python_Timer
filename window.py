cc3384ae42793f6859bad0999e0f513bdb8dd6fe
Paul Kolz <paul.d.kolz@gmail.com>
2026-09-21 21:40:07 -0500

    splits

--- window.py
+++ window.py
@@ -151,7 +151,7 @@ class playgroundClass():
 class bigTimerClass(timerClass):
 
 	def __init__(self, inFontTotal=None, inFontFamily=None, inFontSize=50, inFontColor=None, playground_inHeight=None, playground_inColor=None):
-		super().__init__() 
+		super().__init__()
 
 		# font
 		timerSetup(self=self, inFontTotal=inFontTotal, inFont=inFontFamily, inFontSize=inFontSize)
@@ -192,13 +192,109 @@ class bigTimerClass(timerClass):
 # split stuff
 class splitClass():
 
-	def __init__(self, splitNumber=""):
+	def __init__(self, splitNumber="", inFontTotal=None, inFontFamily=None, inFontSize=25, inFontColor=None, playground_inHeight=None, playground_inColor=None):
 
 		self.type = "split"
 
 		self.name = "split" + str(splitNumber)
 
+		# font
+		timerSetup(self=self, inFontTotal=inFontTotal, inFont=inFontFamily, inFontSize=inFontSize)
+		if inFontColor == None:
+			self.fontColor = preferences["defaultFontColor"]
+		else:
+			self.fontColor = inFontColor
+
 		self.time = None
+
+		self.updateTime()
+
+		if playground_inHeight == None:
+			playground_forHeight = preferences["split_playgroundHeight"]
+			if preferences["split_playgroundHeight"] == "sameasfont":
+				playground_forHeight = max(self.printedName.get_rect().height, self.printedTime.get_rect().height)
+		else:
+			playground_forHeight = playground_inHeight
+
+		if playground_inColor == None:
+			playground_forColor = preferences["split_backgroundColors"][(splitNumber-1)%len(preferences["split_backgroundColors"])]
+		else:
+			playground_forColor = playground_inColor
+
+		self.playground = playgroundClass(playground_forHeight, playground_forColor)
+
+		self.height = self.playground.height
+
+
+
+	def updateTime(self, time=None):
+		if time == None:
+			if self.time == None:
+				wap = 0
+			else:
+				wap = self.time
+		else:
+			wap = time
+
+		flabber = decimal(wap/decimal("1000"))
+
+		secondsTotal = int(flabber)
+		seconds = secondsTotal%60
+
+		minutesTotal = int(secondsTotal/60)
+		minutes = minutesTotal%60
+
+		hoursTotal = int(minutesTotal/60)
+		hours = hoursTotal%24
+
+		daysTotal = int(hoursTotal/24)
+		days = daysTotal%60
+
+
+		startLevel = 0
+		if minutesTotal > 0:
+			startLevel = 1
+		if hoursTotal > 0:
+			startLevel = 2
+		if daysTotal > 0:
+			startLevel = 3
+
+		def ad(l, i, ee):
+			ab = ee
+			if startLevel > l and i < 10:
+				ab += "0"
+			if startLevel >= l:
+				ab += str(i)
+				if l > 0:
+					ab += ","
+
+			return str(ab)
+
+		toReturn = ""
+		toReturn = ad(3, days, toReturn)
+		toReturn = ad(2, hours, toReturn)
+		toReturn = ad(1, minutes, toReturn)
+		toReturn = ad(0, seconds, toReturn)
+		if wap == 0:
+			toReturn = ''
+		else:
+			toReturn += "." + str(flabber).split(".")[1][:2]
+
+		self.printedTime = self.font.render(toReturn, True, self.fontColor)
+		self.printedName = self.font.render(self.name, True, self.fontColor)
+
+
+
+	def draw(self, placement=None):
+		if placement == None:
+			playPlace = (0, 0)
+			forPlacement = (self.printedTime.get_rect(right=preferences["screen_defaultWidth"])[0], 0)
+		else:
+			playPlace = (0, placement)
+			forPlacement = (self.printedTime.get_rect(right=preferences["screen_defaultWidth"])[0], placement)
+		screen.blit(self.playground.actual, playPlace)
+		screen.blit(self.printedName, playPlace)
+		screen.blit(self.printedTime, forPlacement)
 
 
 
@@ -214,6 +310,23 @@ class splitContainerClass():
 			self.splits.append(splitClass(i+1))
 
 		self.splitPointer = 0
+
+		self.findHeight()
+
+
+
+	def findHeight(self):
+		self.height = 0
+		for p in self.splits:
+			self.height += p.playground.actual.get_height()
+
+
+
+	def draw(self, goDown=0):
+		down = 0
+		for s in self.splits:
+			s.draw(goDown+down)
+			down += s.height
 		
 
 
@@ -245,6 +358,8 @@ default_preferences = {
 	"screen_defaultWidth": 400,
 	"bigTimer_backgroundColor": "lavender",
 	"bigTimer_playgroundHeight": "sameasfont",
+	"split_backgroundColors": ["slateGrey"],
+	"split_playgroundHeight": "sameasfont",
 	"defaultFontFamily": "IBMPlexMono-Regular.ttf",
 	"defaultFontColor": "black",
 	"timeOnLeft": False,
@@ -285,10 +400,14 @@ else:
 lastTime = decimal(str(pygame.time.get_ticks()))
 
 bigTimer1 = bigTimerClass()
+splitContainer1 = splitContainerClass()
 
 
 
-screen = pygame.display.set_mode((preferences["screen_defaultWidth"], bigTimer1.printedTime.get_rect().height))
+screenHeight = 0
+screenHeight += bigTimer1.playground.actual.get_height()
+screenHeight += splitContainer1.height
+screen = pygame.display.set_mode((preferences["screen_defaultWidth"], screenHeight))
 pygame.display.set_caption(preferences["timerCaption"])
 screenColor = "black"
 clock = pygame.time.Clock()
@@ -330,7 +449,6 @@ while True:
 
 		# restart
 		if keyboard.is_pressed(preferences["commandHotkeys"]["restart"]) and not keysPressed["restart"]:
-			print("restarted")
 			bigTimer1.restart()
 			keysPressed["restart"] = True
 
@@ -352,6 +470,7 @@ while True:
 	screen.fill(screenColor)
 
 	bigTimer1.draw()
+	splitContainer1.draw(bigTimer1.playground.height)
 
 
 	pygame.display.update()
